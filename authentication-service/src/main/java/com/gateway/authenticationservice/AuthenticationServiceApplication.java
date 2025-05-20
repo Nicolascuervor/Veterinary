2a691a2971cf49362073050dd82fc1279097b6cb
DanielOrozco <danielorozco010704@gmail.com>
2025-05-20 08:32:20 -0500

    commit de prueba

--- authentication-service/src/main/java/com/gateway/authenticationservice/AuthenticationServiceApplication.java
+++ authentication-service/src/main/java/com/gateway/authenticationservice/AuthenticationServiceApplication.java
@@ -9,5 +9,5 @@ public class AuthenticationServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticationServiceApplication.class, args);
 	}
-
+// comment
 }
