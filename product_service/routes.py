24730288609797f987a7bb6f33a977d41a035da2
Xiomara Ocampo <xocampo_165@cue.edu.co>
2025-06-03 08:33:13 -0500

    cambio a mysql

--- product_service/routes.py
+++ product_service/routes.py
@@ -20,6 +20,7 @@ def get_productos():
             'seller_id': p.seller_id
         } for p in productos
     ])
+#ejemplo postman: 
 
 @routes.route('/productos', methods=['POST'])
 def crear_producto():
@@ -36,3 +37,38 @@ def crear_producto():
     db.session.add(producto)
     db.session.commit()
     return jsonify({'message': 'Producto creado con éxito'}), 201
+
+#ejemplo postman.
+#http://localhost:5001/productos 
+'''
+{
+  "name": "Collar para perro",
+  "description": "Collar ajustable para perro de raza mediana.",
+  "price": 12.5,
+  "stock": 100,
+  "image": "https://example.com/collar.jpg",
+  "category_id": 1,
+  "seller_id": 101
+}
+'''
+
+
+@routes.route('/categorias', methods=['POST'])
+def crear_categoria():
+    data = request.json
+    categoria = Category(
+        name=data['name'],
+        description=data.get('description', '')
+    )
+    db.session.add(categoria)
+    db.session.commit()
+    return jsonify({'message': 'Categoría creada con éxito', 'id': categoria.id}), 201
+
+
+#para probar postman ejemplo: 
+#http://localhost:5001/categorias
+
+#{
+  #"name": "Accesorios",
+ # "description": "Productos de tienda para mascotas"
+#}
