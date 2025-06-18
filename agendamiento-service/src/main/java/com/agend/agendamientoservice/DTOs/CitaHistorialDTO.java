be49680ecdf372c1625b201791d862bf72b45339
Nicolás <ncuervo_175@cue.edu.co>
2025-06-18 15:54:07 -0500

    Imagenes de productos

---
+++ agendamiento-service/src/main/java/com/agend/agendamientoservice/DTOs/CitaHistorialDTO.java
@@ -0,0 +1,18 @@
+package com.agend.agendamientoservice.DTOs;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+public class CitaHistorialDTO {
+    private Long id;
+    private LocalDateTime fecha;
+    private String motivoServicio;
+    private String estadoCita;
+    private String veterinarioNombre;
+    private EncuentroClinicoDTO encuentro; // Será null si la cita no se ha realizado
+}
